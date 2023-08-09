d623568b6625c2230c64de1c020a21ed5bbc1078
Rahul Biju <63910868+Rahulbiju003@users.noreply.github.com>
2023-08-09 11:30:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import {useState , useRef} from 'react'
+
+function App() {
+
+  const [todoList,setTodoList] = useState([]);
+  const [currentTask,setCurrentTask] = useState("");
+  const inputTask = useRef(null);
+  const addTask = () =>{
+    setTodoList([...todoList, currentTask]);
+    inputTask.current.value = "";
+    setCurrentTask("");
+  };
+  const deleteTask = (taskToDelete) =>{
+     setTodoList(todoList.filter((task)=>{
+      return(task!== taskToDelete);
+     }))
+  };
+
+  return (
+    <div className="App">
+
+      <h1>To Do List</h1>
+
+      <div>
+
+       <input 
+        ref={inputTask} 
+        type='text' 
+        placeholder='Task' 
+        onChange={(event)=>{
+          setCurrentTask(event.target.value);
+        }}/>
+
+       <button onClick={addTask}>Add Task</button>
+
+      </div>
+
+      <hr/>
+
+      <ul>
+
+       {todoList.map((val,key)=>{
+        return(
+           <div id="task">
+           <li key={key}>{val}</li>
+           <button onClick={()=>deleteTask(val)}>Delete</button>
+           </div>
+          );
+       })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
